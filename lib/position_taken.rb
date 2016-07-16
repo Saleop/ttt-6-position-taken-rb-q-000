63e361795621ea52007f8caaa64d6663ec24c0e3
Ryan Wells <rwellsmail@gmail.com>
2016-07-16 20:13:15 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index) # Returns true if position is taken, false if not.
+  space = board[index] # space is the value of the place on the board indicated by index.
+  space == "X" || space == "O"
+end
